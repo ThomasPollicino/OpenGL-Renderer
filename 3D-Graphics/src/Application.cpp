6827d897f08a932cdd376c8476c443e08a8fe765
alfonsodelr <delrosarioluis22@gmail.com>
2022-09-20 23:30:46 -0700

    Uniforms

--- 3D-Graphics/src/Application.cpp
+++ 3D-Graphics/src/Application.cpp
@@ -123,6 +123,8 @@ int main(void)
 
     glfwMakeContextCurrent(window);
 
+    glfwSwapInterval(3);
+
     if (glewInit() != GLEW_OK)
         std::cout << "Error!" << std::endl;
 
@@ -165,12 +167,26 @@ int main(void)
     unsigned int shader = CreateShader(source.VertexSource, source.FragmentSource);
     GLCall(glUseProgram(shader));
    
+    GLCall(int location = glGetUniformLocation(shader, "u_Color"));
+    ASSERT(location != -1);
+
+    float r = 0.0f;
+    float increment = 0.05f;
     //main loop
     while (!glfwWindowShouldClose(window))
     {
         GLCall(glClear(GL_COLOR_BUFFER_BIT));
         
+        GLCall(glUniform4f(location, r, 0.3f, 0.8f, 1.0f));
+
         GLCall(glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr)); 
+
+        if (r > 1.0f)
+            increment = -0.05f;
+        else if (r < 0.0f)
+            increment = 0.05f;
+
+        r += increment;
 
         GLCall(glfwSwapBuffers(window));
 
